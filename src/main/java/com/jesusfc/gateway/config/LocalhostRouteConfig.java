3f4083c066100527db6ea9a9b5eafb8d75ce7a44
Jesusfc <jfcaraballo@gmail.com>
2024-12-05 11:47:10 +0100

    update microservices

--- src/main/java/com/jesusfc/gateway/config/LocalhostRouteConfig.java
+++ src/main/java/com/jesusfc/gateway/config/LocalhostRouteConfig.java
@@ -24,9 +24,7 @@ public class LocalhostRouteConfig {
         Function<GatewayFilterSpec, UriSpec> uriSpec = f -> f.circuitBreaker(c -> c.setName("service1_circuit_breaker").setFallbackUri("forward:/service-fail").setRouteId("service_breaker_1"));
 
         return builder.routes()
-                .route("service_1", r -> r.path("/service_1/*")
-                        .filters(uriSpec)
-                        .uri("lb://spb3-service-1"))
+                .route("service_1", r -> r.path("/service_1/*").filters(uriSpec).uri("lb://spb3-service-1"))
                 .route("service_2", r -> r.path("/service_2/*").uri("lb://spb3-service-2"))
                 .route("feign_service", r -> r.path("/feign-service/*").uri("lb://spb3-service-2"))
                 .route("circuit_breaker", r -> r.path("/service-fail").uri("lb://spb3-circuit-breaker"))
