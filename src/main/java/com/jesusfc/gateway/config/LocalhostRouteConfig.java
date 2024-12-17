aa4befbdd4f5eb9030cecab6e0caeb022372144b
Jesusfc <jfcaraballo@gmail.com>
2024-12-17 13:50:18 +0100

    update microservices properties

--- src/main/java/com/jesusfc/gateway/config/LocalhostRouteConfig.java
+++ src/main/java/com/jesusfc/gateway/config/LocalhostRouteConfig.java
@@ -25,6 +25,7 @@ public class LocalhostRouteConfig {
 
         return builder.routes()
                 .route("service_1", r -> r.path("/service_1/*").filters(uriSpec).uri("lb://spb3-service-1"))
+                //.route("service_1", r -> r.path("/service_1/*").uri("lb://spb3-service-1"))
                 .route("service_2", r -> r.path("/service_2/*").uri("lb://spb3-service-2"))
                 .route("feign_service", r -> r.path("/feign-service/*").uri("lb://spb3-service-2"))
                 .route("circuit_breaker", r -> r.path("/service-fail").uri("lb://spb3-circuit-breaker"))
